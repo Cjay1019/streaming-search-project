2f795eb577e549d24d1accf89872ce0efa3ad39b
IamMonkie <monkie_225@yahoo.com>
2019-03-30 11:10:30 -0400

    navbar and basic styling added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import OmdbContainer from "./components/OmdbContainer";
+import Navbar from "./components/navbar";
+import Footer from "./components/Footer";
 import "./mdb.css";
 import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="backgroundColor elegant-color ">
+      <Navbar />
       <OmdbContainer />
+      <Footer />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,14 @@ import React from "react";
 
 function Card(props) {
   return (
-    <div className="card text-center">
-      <div className="card-header">
+    <div
+      className="card text-center special-color-dark cyan-text
+    "
+    >
+      <div
+        className="card-header special-color
+#37474F"
+      >
         <h2>{props.heading}</h2>
       </div>
       <div className="card-body">{props.children}</div>
